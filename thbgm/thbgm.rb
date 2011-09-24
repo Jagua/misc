5597212516c8bc1be381ae4b68f39ef8ae024c8a
Jagua <dabura@dabura-desktop.(none)>
2011-09-25 03:31:49 +0900

    add --loop and --filename options

--- thbgm/thbgm.rb
+++ thbgm/thbgm.rb
@@ -3,9 +3,11 @@
 # -*- Mode: Ruby; Encoding: utf8n -*-
 $stdout.sync = true
 $stdout.set_encoding("CP932")
+require 'optparse'
 require 'yaml'
 
-puts "thbgm.rb version 0.1 (2011-09-19)" # version
+Version = '0.2'
+puts "thbgm.rb version #{Version} (2011-09-25)" # version
 
 if RUBY_VERSION<'1.9'
   puts "This script run on ruby1.9. your ruby version is #{RUBY_VERSION}."
@@ -15,42 +17,50 @@ end
 yaml_file = 'thbgm.yaml'
 thbgm_yml = YAML.load_file(yaml_file)
 
-argv = ARGV[0]
-if argv =~/-(th\d+)/
-  th = $1.to_s
-  puts "argv : #{th}"
-else
-  puts "help:"
-  puts "  puts titles-file in the folder \"titles\", and execute this script."
-  puts "  in case you get the wave file of th13, you execute the following command."
-  puts "  > thbgm.rb -th13"
+op = OptionParser.new
+opt={}
+opt[:loop]= 1
+op.on('--th=VALUE', 'th no. (VALUE: 06,07,..,13)') {|v| opt[:th] = 'th'+v}
+op.on('--loop=VALUE', 'loop count. (VALUE: 0-)') {|v| opt[:loop] = v}
+op.on('--track=VALUE', 'track no.') {|v| opt[:track] = v}
+op.on('--omit-subtitle', 'omit subtitle from title') {|v| opt[:omitsubttl] = v}
+op.on('--filename=VALUE', 'filename template. (default: "%th%_%track% %title%.wav")') {|v| opt[:filename] = v}
+op.parse!(ARGV)
+
+unless opt[:th]
+  puts op.help
   exit
 end
 
-titles_file = "titles/titles_#{th}.txt"
-thbgm = thbgm_yml["thbgm"][th]
-
-#filename_template = "%th%_%2d %title%"
+titles_file = "titles/titles_#{opt[:th]}.txt"
+thbgm = thbgm_yml["thbgm"][opt[:th]]
+default_filename = "%th%_%track% %title%.wav"
 
 if File.exist?(titles_file) and File.exist?(thbgm)
   list = open(titles_file, "r").lines.select{|s| s !~ /^(?:\#|@)/}
-  #puts list
   dat = open(thbgm, "rb").read
 
   i="00"
-  list.each do |ln|
-    offset, intro, loop, title = ln.strip.split(/,/).map{|i|
-      i=~/^[0-9a-fA-F]+$/?i.to_i(16):i.encode("CP932")
-    }
-    size = intro + loop
-    filename = "#{th}_#{i} #{title}.wav"
+  list.each do |ln0|
+    ln = ln0.encode(Encoding::UTF_8)
+    ln =~ /^([0-9a-fA-F]+),([0-9a-fA-F]+),([0-9a-fA-F]+),([^　]+.*?)(　?.*?)$/
+    offset = $1.to_s.to_i(16)
+    intro = $2.to_s.to_i(16)
+    loop = $3.to_s.to_i(16)
+    title = $4.strip
+    subtitle = $5.strip
+    title += subtitle unless opt[:omitsubttl]
+    size = intro + loop*opt[:loop].to_i
+    filename = opt[:filename]?opt[:filename]:(default_filename)
+    filename = filename.gsub(/%.*?%/, {'%th%'=>opt[:th], '%track%'=>i, '%title%'=>title})
     riffheader = ["RIFF", size+44-8, "WAVEfmt ",  16, 1, 2, 44100, 44100*2*2, 2*2, 16, "data", size].pack("A4VA8VvvVVvvA4V")
     open(filename, "wb"){|f|
       f.write riffheader
-      f.write dat[offset, size]
+      f.write dat[offset, intro]
+      f.write dat[offset + intro, loop]*opt[:loop].to_i
     }
     i.succ!
-    print file + "... done.\n"
+    print filename + "... done.\n"
   end
 end
 
